bd010afcaafad4f197f39788936e171597e11c44
Rachel Lew <rlew07@gmail.com>
2020-03-21 14:47:54 -0600

    Add annotations for readingList.jsx

--- app/javascript/packs/readingList.jsx
+++ app/javascript/packs/readingList.jsx
@@ -1,13 +1,22 @@
+/* Preact provides an h() function that turns JSX into virtual DOM elements */
+/* Preact also provides a render() function that creates a DOM tree from that virtual DOM */
 import { h, render } from 'preact';
+/* Imports a function that gets user data and that user's CSRF (cross site request forgery) token */
 import { getUserDataAndCsrfToken } from '../chat/util';
+/* Imports readingList/readingList.jsx */
 import { ReadingList } from '../readingList/readingList';
 
+/* Invokes a function called loadElement that calls getUserDataAndCsrfToken function */
 function loadElement() {
+  /* get getUserDataAndCsrfToken returns a promise/returns the current user. */
   getUserDataAndCsrfToken().then(({ currentUser }) => {
+    /* Sets a variable called root to the reading list element on the DOM */
     const root = document.getElementById('reading-list');
+    /* If the root (the reading list element) exists, render the ReadingList */
     if (root) {
       render(
         <ReadingList
+          /* The ReadingList element contains availableTags (the tags that the current user follows) and the status of the articles on the reading list. The status is either archived or valid. */
           availableTags={currentUser.followed_tag_names}
           statusView={root.dataset.view}
         />,
@@ -18,6 +27,8 @@ function loadElement() {
   });
 }
 
+/* When a user hovers over 'change', InstantClick will prefetch the page while the user is hovering over the hyperlink
+so that when they click on it, it's instantaneous. In this case, it prefetches the ReadingList element */
 window.InstantClick.on('change', () => {
   loadElement();
 });
